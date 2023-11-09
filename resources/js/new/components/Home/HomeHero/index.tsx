4ce0124078fc2e30cb80cc1046c6503b63c2e0e0
Panto! <guustavopantoja@gmail.com>
2023-11-09 15:23:17 -0300

    feat: add home hero breakpoint

--- resources/js/new/components/Home/HomeHero/index.tsx
+++ resources/js/new/components/Home/HomeHero/index.tsx
@@ -7,27 +7,35 @@ import Button from '@/new/components/Button';
 
 const HomeHero: React.FC = () => {
   return <div className={styles.hero}>
-    <Header mode="transparent"/>
-    
-    <div className={styles.title}>
-      <div className={styles.titleStatic}>Play on</div>
-      <div className={styles.instrumentSection}>Pro Guitar</div>
-    </div>
+    <Header mode="transparent" />
 
-    <div className={styles.buttons}>
-      <Button color="blue">
-        Download
-      </Button>
+    <div className={styles.content}>
+      <div className={styles.about}>
+        <div className={styles.title}>
+          <div className={styles.titleStatic}>Play on</div>
+          <div className={styles.instrumentSection}>Pro Guitar</div>
+        </div>
 
-      <Button>
-        <Github />
-        Check our Repository
-      </Button>
-    </div>
+        <span className={styles.description}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+        </span>
 
-    <div className={styles.video}>
-      <div className={styles.gem} data-color="green"></div>
-      <div className={styles.gem} data-color="blue"></div>
+        <div className={styles.buttons}>
+          <Button color="blue">
+            Download
+          </Button>
+
+          <Button>
+            <Github />
+            Check our Repository
+          </Button>
+        </div>
+      </div>
+
+      <div className={styles.video}>
+        <div className={styles.gem} data-color="green"></div>
+        <div className={styles.gem} data-color="blue"></div>
+      </div>
     </div>
 
   </div>;
