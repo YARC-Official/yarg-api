054a94b302364d24ad3061ba305aab58ea76db39
danielhe4rt <hey@danielheart.dev>
2024-01-28 19:38:58 -0300

    feat: recovery and validate token

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Auth\LoginRequest;
+use App\Http\Requests\V1\Auth\RecoveryRequest;
 use App\Http\Requests\V1\Auth\RegisterRequest;
+use App\Models\User;
 use App\Services\AuthService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function __construct(private readonly AuthService $service)
+    public function __construct(
+        private readonly AuthService $service
+    )
     {
     }
 
-    public function postRegister(RegisterRequest $request)
+    public function postRegister(RegisterRequest $request): JsonResponse
     {
         return response()->json(
             $this->service->register($request->validated()),
@@ -22,10 +28,32 @@ class AuthController extends Controller
         );
     }
 
-    public function postLogin(LoginRequest $request)
+    public function postLogin(LoginRequest $request): JsonResponse
     {
         return response()->json(
             $this->service->authenticate($request->toDTO())
         );
+    }
+
+    public function postRecovery(RecoveryRequest $request): Response
+    {
+        $this->service->sendRecovery($request->validated());
+
+        return response()->noContent();
+    }
+
+    public function getTokenValidation(
+        Request $request,
+        string  $token
+    ): Response
+    {
+        try {
+            $this->service->validateToken($token, $request->input('email'));
+            return response()->noContent();
+        } catch (\Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], Response::HTTP_BAD_REQUEST);
+        }
+
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,8 @@ Route::prefix('v1')->group(function() {
     Route::prefix('auth')->group(function () {
         Route::post('register', [AuthController::class, 'postRegister'])->name('v1.auth.register');
         Route::post('login', [AuthController::class, 'postLogin'])->name('v1.auth.login');
+        Route::post('recovery', [AuthController::class, 'postRecovery'])->name('v1.auth.recovery');
+        Route::get('/validate-token/{token}', [AuthController::class, 'getTokenValidation'])->name('v1.auth.validate-token');
     });
 });
 
