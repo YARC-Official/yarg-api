80e9a17d5907f2b613efcacfc8717bbd7ffb447c
danielhe4rt <hey@danielheart.dev>
2024-01-28 14:21:31 -0300

    chore: clean up

--- resources/js/Hooks/useTypedPage.ts
+++
@@ -1,6 +0,0 @@
-import { usePage } from '@inertiajs/react';
-import { InertiaSharedProps } from '@/types';
-
-export default function useTypedPage<T = {}>() {
-  return usePage<InertiaSharedProps<T>>();
-}
--- resources/js/new/components/Icons/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Discord } from "./Discord";
-export { default as Github } from "./Github";
-export { default as Instagram } from "./Instagram";
-export { default as Reddit } from "./Reddit";
-export { default as TikTok } from "./TikTok";
-export { default as Twitch } from "./Twitch";
-export { default as X } from "./X";
-export { default as Youtube } from "./Youtube";
